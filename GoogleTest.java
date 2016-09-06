7bad20b7748364feee4a7e8f1f81797ad63da3d6
Purushoth88 <purush_it2007@yahoo.co.in>
2016-09-06 12:22:23 +0000

    Committed GoogleTest.java.

---
+++ GoogleTest.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class GoogleTest {
+    public static void main(String[] args) throws Exception {
+        FirefoxDriver wd;
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("https://www.google.co.in/?gfe_rd=cr&ei=oLTOV4W2Ks-CoAOI-I-IAg&gws_rd=ssl");
+        wd.findElement(By.id("lst-ib")).click();
+        wd.findElement(By.id("lst-ib")).clear();
+        wd.findElement(By.id("lst-ib")).sendKeys("github");
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
